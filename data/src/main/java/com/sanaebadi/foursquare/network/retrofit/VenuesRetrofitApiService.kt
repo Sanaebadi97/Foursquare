2c87e1047ca2706e4a269a91f1bc242a43412d29
Sana Ebadi <sanaebadi97@gmail.com>
2020-11-29 21:34:51 +0330

    added Api service of retrofit for around places

---
+++ data/src/main/java/com/sanaebadi/foursquare/network/retrofit/VenuesRetrofitApiService.kt
@@ -0,0 +1,19 @@
+package com.sanaebadi.foursquare.network.retrofit
+
+import com.sanaebadi.data.BuildConfig
+import io.reactivex.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface VenuesRetrofitApiService {
+    @GET(value = "${BuildConfig.API_BASE_URL}venues/search")
+    fun aroundVenues(
+        @Query("ll") latLong: String,
+        @Query("client_id") clientId: String,
+        @Query("client_secret") clientSecret: String,
+        @Query("v") versioning: String,
+        @Query("intent") intent: String,
+        @Query("radius") radius: Int,
+        @Query("limit") limit: Int
+    ): Flowable<>
+}
